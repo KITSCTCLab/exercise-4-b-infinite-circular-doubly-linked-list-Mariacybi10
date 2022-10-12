8af874bbea7ba3479b7eaff924f576168024093e
Mariacybi10 <91770304+Mariacybi10@users.noreply.github.com>
2022-10-12 09:41:45 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,13 +3,21 @@ length_of_circular_linked_list = int(input())
 # Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
 circular_linked_list = list(map(int,input().strip().split(" ")))
 # Write your code here
-output=[]
+if len(circular_linked_list)%3 !=0:
+    while(len(circular_linked_list)%3 !=0):
+        circular_linked_list.append(-1)
+temp = []
+index=-1
+for i in range(len(circular_linked_list)//3):
+    x = []
+    for i in range(3):
+        index+=1
+        if circular_linked_list[index] == -1:
+            continue
+        x.append(circular_linked_list[index])
+    for j in x:
+        if j not in temp:
+            temp.append(j)
 
-for x in circular_linked_list:
-    if x not in output:
-        output.append(x)
-        
-print(len(output))
-
-for x in output:
-    print(x,end=" ")
+print(len(temp))
+print(*temp,sep=" ")
